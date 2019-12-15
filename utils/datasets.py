5849c4adbbd32403db5f61865aaaeca79baefeb7
johanneshoetter <57453680+johanneshoetter@users.noreply.github.com>
2019-12-15 20:18:29 +0100

    new settings

--- utils/datasets.py
+++ utils/datasets.py
@@ -46,3 +46,27 @@ class AlphabetSortingDataset(Dataset):
     
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx], self.chars[idx]
+    
+class NumberSortingDataset(Dataset):
+    
+    def __init__(self, num_samples, min_num=0, max_num=9, min_len=20, max_len=60):
+        self.num_samples = num_samples
+        self.min = min_num
+        self.max = max_num
+        self.min_len = min_len
+        self.max_len = max_len
+        self.x, self.y = self._build()
+        
+    def _build(self):
+        array_len = torch.randint(low=self.min_len, 
+                            high=self.max_len + 1,
+                            size=(1,))
+        x = torch.randint(low=self.min, high=self.max, size=(self.num_samples, array_len))
+        y = x.argsort()
+        return x, y
+    
+    def __len__(self):
+        return len(self.x)
+    
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
