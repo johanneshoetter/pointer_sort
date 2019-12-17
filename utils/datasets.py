fb8e37061bf65dc66312213461a5415d1d907990
johanneshoetter <57453680+johanneshoetter@users.noreply.github.com>
2019-12-18 00:52:03 +0100

    Working version of schema matching

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,9 +1,13 @@
 from torch.utils.data import Dataset
 import torch
-import pickle
+#import pickle
 from .preprocessing import WordEmbedding, load_word_emb
 import numpy as np
 import os
+import pandas as pd
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Subset
 
 w2v_config = {
     'data_dir': 'data/glove',
@@ -55,14 +59,14 @@ class AlphabetSortingDataset(Dataset):
         return self.x[idx], self.y[idx], self.chars[idx]
     
     def save(self, path):
-        chunk = {
+        data = {
             'x': self.x,
             'y': self.y,
             'chars': self.chars,
             'min_len': self.min_len,
             'max_len': self.max_len
         }
-        torch.save(chunk, os.path.join(path, 'alphabet.pt'))
+        torch.save(data, os.path.join(path, 'alphabet.pt'))
 
     def load(self, path):
         self.x, self.y, self.chars = [], [], []
@@ -72,6 +76,72 @@ class AlphabetSortingDataset(Dataset):
         self.y.extend(y)
         self.chars.extend(chars) 
         return data['min_len'], data['max_len']
+    
+    def split(self, train_ratio):
+        train_idx, test_idx = train_test_split([idx for idx in range(self.__len__())], test_size=1 - train_ratio)
+        return (Subset(self, train_idx), Subset(self, test_idx))
+    
+    
+class SchemaMatchingDataset(Dataset):
+    
+    def __init__(self, schema_matches: pd.DataFrame, from_path=False):
+        self.w2v = WordEmbedding(load_word_emb(w2v_config['data_dir'], 
+                                               w2v_config['word2idx_path'],
+                                               w2v_config['usedwordemb_path']))
+
+        self.x_raw, self.x, self.y = [], [], []
+        if not from_path:
+            self._load_from_df(schema_matches)
+            
+    def _load_from_df(self, schema_matches):
+        for idx, row in tqdm(schema_matches.iterrows(), total=len(schema_matches)):
+            source_col, input_cols, target_col = row
+            source_col, target_col = source_col.lower(), target_col.lower()
+            input_cols = ';'.join([col.lower() for col in input_cols])
+            input_sequence = "<BEG>;{};<SEQ>;{};<END>".format(source_col, input_cols)
+            input_sequence_tok = input_sequence.split(';')
+            end_pointer = input_sequence_tok.index('<END>') # basically len(input_sequence_tok)
+            padding =  [end_pointer for _ in range(len(input_sequence_tok)-1)]
+            embeddings = []
+            try:
+                target_pointer = torch.Tensor([input_sequence_tok.index(target_col)] + padding).long()
+                # e.g. tokens = ['<BEG>', 'area (km 2 )', '<SEQ>', 'dvd title',  'region 2', 'region 1 (us)', '<END>']
+
+                for token in input_sequence_tok:
+                    embedding = np.mean([self.w2v(word) for word in token], axis=0)
+                    embeddings.append(embedding)
+                embeddings = torch.Tensor(embeddings)
+                
+                self.x.append(embeddings)
+                self.x_raw.append(input_sequence)
+                self.y.append(target_pointer)
+            except:
+                continue
+               
+    def __len__(self):
+        return len(self.x_raw)
+    
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx], self.x_raw[idx]
+    
+    def save(self, path):
+        data = {
+            'x': self.x,
+            'x_raw': self.x_raw,
+            'y': self.y
+        }
+        torch.save(data, os.path.join(path, 'schema_matching.pt'))
+    
+    def load(self, path):
+        data = torch.load(os.path.join(path, 'schema_matching.pt'))
+        x, x_raw, y = data['x'], data['x_raw'], data['y']
+        self.x.extend(x)
+        self.x_raw.extend(x_raw)
+        self.y.extend(y)
+        
+    def split(self, train_ratio):
+        train_idx, test_idx = train_test_split([idx for idx in range(self.__len__())], test_size=1 - train_ratio)
+        return (Subset(self, train_idx), Subset(self, test_idx))
         
 #class AlphabetSortingDataset(Dataset):
 #    
